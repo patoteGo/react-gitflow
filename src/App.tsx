ae5a0e8f9270be7fe9785d8f7d6ee18426cfbaf7
Patricio Gonzalez <patote.gonzalez@gmail.com>
2020-09-13 18:43:43 -0300

    arreglado

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> feature1
+          Edit <code>src/App.tsx</code> feature1 arreglado
         </p>
         <a
           className="App-link"
